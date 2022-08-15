5001e558eb5eef8e3019c9fbd682e9509fbe6b4d
James <5511220+Zamiell@users.noreply.github.com>
2022-08-15 09:49:20 -0400

    cleanup

--- packages/library-b/dist/library-a/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.LIB_A = void 0;
-exports.LIB_A = 123;
--- packages/library-b/dist/library-b/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var library_a_1 = require("library-a");
-console.log(library_a_1.LIB_A);
-someGlobalFunction();
