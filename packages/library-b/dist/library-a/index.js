b8ddb455c256dcbe6d6d56aa6b3e2365e15ccf21
James <5511220+Zamiell@users.noreply.github.com>
2022-08-15 09:02:47 -0400

    refactoring

---
+++ packages/library-b/dist/library-a/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.LIB_A = void 0;
+exports.LIB_A = 123;
---
+++ packages/library-b/dist/library-b/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var library_a_1 = require("library-a");
+console.log(library_a_1.LIB_A);
+someGlobalFunction();
